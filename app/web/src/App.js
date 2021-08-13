61eadcc019228a93d9fb8c644f1016fdf5d4d629
gambitmis <gambitmis@gmail.com>
2021-08-13 19:10:52 +0000

    tried

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Navbar from './Navbar';
 import Input from './Input';
+import { useState } from 'react';
+import Post from './Post';
+
+let id = 1;
 
 function App() {
+  const [posts,setPosts] = useState([]);
+
+  function addPost(title){
+    const newPost = { id, title }
+    setPosts([newPost, ...posts]);
+    id += 1;
+  }
   return (
     <div className="App">
         <Navbar/>
-        <Input/>
+       <Input addPost={addPost} />
+       {posts.map((post) => (<Post id={post.id} title={post.title}/> ))};
     </div>
   );
 }
---
+++ app/web/src/Post.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Post({ id,title }){
+    return (
+        <div className="Post">
+            <div className="Post_title">{title}</div>
+            <img 
+                className="Post_Image" 
+                src="https://source.unsplash.com/random?sig=${id}" 
+            />
+        </div>
+        
+    );
+}
+
+Post.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired
+};
+
+export default Post
