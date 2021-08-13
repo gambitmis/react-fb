35c14871a83cd396d043cb8fb49277c72efc714a
gambitmis <gambitmis@gmail.com>
2021-08-13 14:29:02 +0000

    input

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import Input from './Input';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Input/>
     </div>
   );
 }
---
+++ app/web/src/Input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function input() {
+    return (
+        <div className="input">
+            <div className="Input_header">Create Post</div>
+            <input
+                className="Input_field"
+                type="text"
+            />
+        </div>
+    );
+}
+
+export default Input;
